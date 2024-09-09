9e65f6deedeeda67a5dda5b5a05be022aa2ab5b9
Falvarez23 <55001660+Falvarez23@users.noreply.github.com>
2024-09-09 15:12:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 
 # Título de la aplicación
-st.title("Simulación de Input de Datos desde Excel")
+st.title("Simulación de Input de Datos desde CSV")
 
-# Subir archivo Excel
-uploaded_file = st.file_uploader("Sube tu archivo Excel para procesarlo como input", type=["csv"])
+# Subir archivo CSV
+uploaded_file = st.file_uploader("Sube tu archivo CSV para procesarlo como input", type=["csv"])
 
 if uploaded_file is not None:
-    # Leer el archivo Excel
-    df = pd.read_excel(uploaded_file)
+    # Leer el archivo CSV
+    df = pd.read_csv(uploaded_file)
 
     # Simular el uso de los datos como "input" en otra aplicación
     st.write("Datos recibidos como 'input':")
@@ -20,7 +20,6 @@ if uploaded_file is not None:
     st.write("Procesando los datos...")
 
     # Ejemplo de algún procesamiento simple
-    # Aquí puedes agregar tu propia lógica para procesar los datos del archivo Excel
     st.write("Resumen de los datos:")
     st.write(df.describe())
 
