37ec7323b3564166a5e3e2fa9c3168bcc60749e3
Falvarez23 <55001660+Falvarez23@users.noreply.github.com>
2024-09-11 14:34:06 -0500

    Update graphics.py

--- utils/graphics.py
+++ utils/graphics.py
@@ -4,10 +4,12 @@ def generate_plotly_chart(data):
     labels = list(data.keys())
     values = list(data.values())
 
-    fig = go.Figure([go.Bar(x=labels, y=values, marker_color='lightskyblue')])
+    fig = go.Figure([go.Bar(x=labels, y=values, marker_color='#467BE9')])
     fig.update_layout(
         title="Indicadores de la Industria",
         xaxis_title="Aspectos",
-        yaxis_title="Porcentaje"
+        yaxis_title="Porcentaje",
+        plot_bgcolor="#FDE192",  # Fondo amarillo
+        font=dict(color="#000000")  # Texto en negro
     )
     return fig
