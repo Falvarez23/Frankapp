dfccae0b9dcd5ac83634f029d42035713d595b6a
Falvarez23 <55001660+Falvarez23@users.noreply.github.com>
2024-09-11 14:39:10 -0500

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -7,10 +7,22 @@ def show_test_page():
     questions = [
         "¿Te sientes atraído por el campo de la robótica y la automatización?",
         "¿Te atrae la idea de utilizar herramientas digitales para expresar tu creatividad?",
+        "¿Te entusiasma la idea de contribuir al desarrollo de videojuegos y experiencias interactivas?",
+        "¿Te apasiona la idea de mejorar la salud y el bienestar de las personas?",
+        "¿Te sientes motivado/a por trabajar en organizaciones sin fines de lucro dedicadas a problemas sociales?",
+        "¿Te apasiona la idea de crear tu propio negocio o trabajar en un entorno dinámico?",
+        "¿Te llama la atención el mundo del comercio electrónico?",
+        "¿Te apasiona la idea de contribuir a la educación de las personas?",
+        "¿Te resulta sorprendente el mundo del marketing y la publicidad?",
+        "¿Te apasiona la idea de trabajar en la creación y diseño de prendas de moda?"
     ]
 
     # Opciones de respuesta
-    options = ["Bajo interés", "Interés moderado", "Alto interés"]
+    options = [
+        "Bajo interés o potencial en la subárea.",
+        "Interés o potencial moderado en la subárea.",
+        "Alto interés o potencial en la subárea."
+    ]
 
     if 'step' not in st.session_state:
         st.session_state.step = 0
@@ -21,10 +33,16 @@ def show_test_page():
         if st.button("Comenzar Test", key="start_test"):
             st.session_state.step = 1
 
-    # Preguntas del test
+    # Preguntas del test con barra de progreso
     elif st.session_state.step > 0 and st.session_state.step <= len(questions):
         question_idx = st.session_state.step - 1
         st.markdown(f"<h2 style='color:#467BE9;'>Pregunta {st.session_state.step} de {len(questions)}</h2>", unsafe_allow_html=True)
+
+        # Barra de progreso
+        progress = (st.session_state.step - 1) / len(questions)
+        st.progress(progress)
+
+        # Pregunta actual
         st.write(questions[question_idx])
         st.radio("Selecciona una opción:", options, key=f"response_{question_idx}")
 
@@ -34,3 +52,6 @@ def show_test_page():
     else:
         st.session_state.step = 0
         st.write("Gracias por completar el test.")
+        st.write("Haz clic en el botón a continuación para ver tus resultados.")
+        if st.button("Ver Resultados"):
+            st.session_state.step = len(questions) + 1  # Avanzar a la página de resultados
